5fa5e099f4912b36da7b148a6b0e62e73416b508
pgrasic <pgrasic98@gmail.com>
2024-09-08 15:12:11 +0200

    Update index.js

--- Grasic JVL DZ/index.js
+++ Grasic JVL DZ/index.js
@@ -6,7 +6,7 @@ lista.dodajNaKraj(2)
 lista.dodajNaPocetak(6)
 lista.dodajNaPocetak(1)
 
-console.log(3,lista.pronadji(3))
+console.log(4,lista.pronadji(4))
 //if(!lista.pronadji(7)) lista.dodajNaKraj(7)
 
 lista.printList()
